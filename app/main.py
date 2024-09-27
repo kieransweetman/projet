91a2d2da5a5d52983aaea55f7aaab09476c83c18
kieransweetman <kikisweetman@gmail.com>
2024-09-27 15:49:30 +0200

    schema proposal

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
-from typing import Union
-
+# all the imports
 from fastapi import FastAPI
 
+# creating a server with python FastAPI
 app = FastAPI()
+
+
+# hello world endpoint
+@app.get("/")
+def read_root():  # function that is binded with the endpoint
+    return {"Hello": "World"}
