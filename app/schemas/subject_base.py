2049ffd9dbca44a6138de4d4db809751b73dba8e
Fabrice3172 <fabrice310981@gmail.com>
2024-09-30 11:35:52 +0200

    subject pydantic schema

---
+++ app/schemas/subject_base.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Optional
+
+
+config = ConfigDict(
+    arbitrary_types_allowed=True,
+    json_schema_extra={
+        "example": {
+            "name": "Mathematics"
+        }
+    }
+)
+
+class SubjectBase(BaseModel):
+    model_config = config
+    name: str
+
+
+class SubjectCreate(SubjectBase):
+    pass
+
+
+class SubjectUpdate():
+    name:Optional[str] = None
