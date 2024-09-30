60a625163ce8cf29097c2c518b449724f0f75168
Fabrice3172 <fabrice310981@gmail.com>
2024-09-30 14:59:17 +0200

    mis à jour schémas mongodb

--- app/models/class.py
+++ app/models/class.py
@@ -3,6 +3,7 @@ schema = {
             "bsonType": "object",
             "required": [ "name" ],
             "properties": {
+                "_id": { "bsonType": "objectId" },
                 "name": {
                     "bsonType": "string",
                     "description" : "must be a string and is required"
