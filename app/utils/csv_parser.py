1b328931e76558e0408286c2a8d2f7c4d9ac9159
kieransweetman <kikisweetman@gmail.com>
2024-09-30 10:56:44 +0200

    adding csv parser and database init for migrating data

---
+++ app/utils/csv_parser.py
@@ -0,0 +1,123 @@
+import os
+import csv
+from pymongo.database import Collection, Database as PyMongoDatabase
+from bson import ObjectId
+from schemas.teacher_base import TeacherBase
+from schemas.student_base import StudentBase
+from datetime import datetime
+
+from config.database import Database
+
+db = Database.get_db()
+
+
+def parse_student(line):
+    date = datetime.strptime(line[4], "%Y-%m-%d %H:%M:%S.%f")
+
+    student = StudentBase(
+        nom=line[1],
+        prenom=line[2],
+        date_naissance=date,
+        adresse=line[5],
+        sexe=line[6],
+        original_id=int(line[0]),
+    )
+
+    return student.model_dump()
+
+
+def parse_teacher(line):
+    date = datetime.strptime(line[3], "%Y-%m-%d %H:%M:%S.%f")
+
+    teacher = TeacherBase(
+        nom=line[1],
+        prenom=line[2],
+        date_naissance=date,
+        sexe=line[4],
+        adresse=line[5],
+        original_id=int(line[0]),
+    )
+
+    return teacher.model_dump()
+
+
+def parse_class(line):
+    teacher = db["teacher"].find_one({"original_id": int(line[2])})
+    id = teacher["_id"]
+    return {"nom": line[1], "teacher": {"_id": id}}
+
+
+def parse_subject(line):
+    return {
+        "nom": line[1],
+    }
+
+
+def parse_grade(line):
+    date_saisie_string = line[1]
+    date = datetime.strptime(date_saisie_string, "%Y-%m-%d %H:%M:%S.%f")
+    return {
+        "date_saisie": date,
+        "note": float(line[7]),
+        "avis": line[8],
+        "avancement": float(line[9]),
+        "original_id": int(line[0]),
+    }
+
+
+def parse_trimester(line):
+    return {
+        "nom": line[1],
+        "date": line[2],
+    }
+
+
+csvs = [
+    {"name": "teacher.csv", "process": parse_teacher},
+    {"name": "student.csv", "process": parse_student},
+    {"name": "class.csv", "process": parse_class},
+    {"name": "subject.csv", "process": parse_subject},
+    {"name": "trimester.csv", "process": parse_trimester},
+    {"name": "grade.csv", "process": parse_grade},
+]
+
+
+def main():
+    csv_dir = "config/csv"
+    from config.database import Database
+
+    db = Database.get_db()
+
+    for file_obj in csvs:
+        file_name = file_obj["name"]
+        name = file_name.split(".")[0]
+        process = file_obj["process"]
+        collection: Collection = db[name]
+
+        if process is None:
+            continue
+
+        with open(os.path.join(csv_dir, file_name), "r") as f:
+            reader = csv.reader(f)
+
+            # skip head
+            next(reader)
+
+            print(
+                "\n##################\n",
+                "Processing: ",
+                file_name,
+                "\n##################\n",
+            )
+
+            try:
+                for line in reader:
+                    model = process(line)
+                    collection.insert_one(model)
+
+            except Exception as e:
+                raise RuntimeError(f"Error while parsing {file_name}: {e}")
+
+    # create a file to indicate that we have processed the csv files
+    with open(os.path.join(csv_dir, "../", "processed.txt"), "a") as f:
+        f.write(f"{True}")
