a147739dbf022c68c8ff40c94debee1c5f855e35
kieransweetman <kikisweetman@gmail.com>
2024-09-30 09:57:40 +0200

    deps

--- app/routes/student.py
+++ app/routes/student.py
@@ -5,14 +5,18 @@ from fastapi import APIRouter, Depends, HTTPException
 from config.database import Database
 from pymongo.database import Database as PyMongoDatabase
 
+from schemas.student_base import StudentCollection
+
 
 router = APIRouter(prefix="/students", tags=["students"])
 
 
-@router.get("/", response_model=List[dict], response_description="Get all students")
+@router.get(
+    "/", response_model=StudentCollection, response_description="Get all students"
+)
 def get_students(db: PyMongoDatabase = Depends(Database.get_db)):
     try:
-        students = db["eleve"].find().to_list()
+        students = StudentCollection(students=db["student"].find().to_list())
         return students
 
     except Exception as e:
